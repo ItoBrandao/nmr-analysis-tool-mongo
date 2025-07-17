f2736ea0a670184884e8cdd0d8a9a63ce034a289
ItoBrandao <pedrofsbrandao@hotmail.com>
2025-07-17 02:35:04 +0100

    Refactored Flask static file serving with /static folder

--- database.py
+++ database.py
@@ -622,3 +622,5 @@ if __name__ == '__main__':
     # Render automatically sets the PORT environment variable
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+#1
